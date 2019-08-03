d4a091e7ab9974623b5532f9316ae7b55576e00c
danish25anroid <danish25android@gmail.com>
2019-08-03 10:48:47 -0700

    second commit

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -20,6 +20,6 @@ from word import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home, name="home"),
+    path('',views.homepage, name="home"),
     path('count/',views.count, name="count"),
 ]
